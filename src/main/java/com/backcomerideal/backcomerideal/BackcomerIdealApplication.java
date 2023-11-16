e5d3290bdc4bd2722dd828234281b68e9a8aa743
Abdessamad belkhialat <abde.ripo@gmail.com>
2023-11-16 14:12:51 +0100

    springboot aplication

---
+++ src/main/java/com/backcomerideal/backcomerideal/BackcomerIdealApplication.java
@@ -0,0 +1,13 @@
+package com.backcomerideal.backcomerideal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackcomerIdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackcomerIdealApplication.class, args);
+	}
+
+}
